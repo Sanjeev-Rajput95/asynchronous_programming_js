1f8be93420bdc0f5e3658fad010a92d1dc6e652d
unknown <sanjeevsingh.77d@gmail.com>
2021-09-13 00:24:56 +0530

    fined the address of prince

--- newcsvFile.js
+++ newcsvFile.js
@@ -2,14 +2,17 @@ const readFile = require('./csvFileReader')
 let path = 'C:/Users/naps/Desktop/snjv/reactapp/aynch_programming_js/Audience.csv';
 readFile(path).then((a) => {
     for(let i = 0 ; i < a.length-1 ; i++) {
-         if("FTA4" == a[i].fname) {
-          console.log(" the last name of person where the first name is FTA4 = " + a[i].lname);
-      }
-     if(a[i].fname = "FTA20") {
-          console.log(" the fname  is  = " + a[i].fname)
-     }
-     if(a[i].fname == "Mandeep") {
-         console.log(a[i])
+    //      if("FTA4" == a[i].fname) {
+    //       console.log(" the last name of person where the first name is FTA4 = " + a[i].lname);
+    //   }
+    //  if(a[i].fname = "FTA20") {
+    //       console.log(" the fname  is  = " + a[i].fname)
+    //  }
+    //  if(a[i].fname == "Mandeep") {
+    //      console.log(a[i])
+    //  }
+     if(a[i].fname == "prince") {
+         console.log(" fined the address of prince = " + a[i].address)
      }
 
      }
